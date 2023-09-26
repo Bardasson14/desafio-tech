cf8dbf4889b081a248ede2f3b454558d3b5475cd
Vitor Bardasson <vbardasson@id.uff.br>
2023-09-26 09:02:01 -0300

    Removendo TODO

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,7 +12,7 @@ module Users
     end
 
     def respond_to_on_destroy
-      if request.headers['Authorization'].present? # TODO: - revisar
+      if request.headers['Authorization'].present?
         jwt_payload = JWT.decode(
           request.headers['Authorization'].split(' ').last,
           Rails.application.credentials.devise_jwt_secret_key!
